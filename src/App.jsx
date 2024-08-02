bf80fab78c6f7991164f52bcb6f50eaf5a8a2509
Anthony A <anthony@Anthonys-MacBook-Pro.local>
2024-08-02 01:26:51 -0400

    finished up feature cards

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,33 @@ import Button from "./components/Shared/Button";
 import Navigation from "./components/Navigation/Navigation";
 import Divider from "./components/Shared/Divider";
 import { IoMdArrowRoundUp } from "react-icons/io";
+import {
+  TbMoodDollar,
+  TbUserHeart,
+  TbUsersGroup,
+  TbHeartFilled,
+} from "react-icons/tb";
+
 import "./app.css";
 import "./reset.css";
 
 function App() {
   const [showIntro, setShowIntro] = useState(false);
   const [tabletHovered, setTabletHovered] = useState(false);
-  const [mouseX, setMouseX] = useState(0);
-  const [mouseY, setMouseY] = useState(0);
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
 
-  const handleMouseMove = (event) => {
-    setMouseX(event.clientX);
-    setMouseY(event.clientY);
-  };
+  // const [mouseX, setMouseX] = useState(0);
+  // const [mouseY, setMouseY] = useState(0);
 
-  const handleMouseLeave = () => {
-    setMouseX(0);
-    setMouseY(0);
-  };
+  // const handleMouseMove = (event) => {
+  //   setMouseX(event.clientX);
+  //   setMouseY(event.clientY);
+  // };
+
+  // const handleMouseLeave = () => {
+  //   setMouseX(0);
+  //   setMouseY(0);
+  // };
 
   return (
     <main className="content-grid">
@@ -34,7 +43,7 @@ function App() {
       <header className="content-grid full-width">
         <div className="grid">
           <ScrollAnimation
-            animateIn="bounceInLeft"
+            animateIn="fadeInLeft"
             animateOnce={true}
             duration={0.8}
           >
@@ -82,19 +91,19 @@ function App() {
             </div>
           </ScrollAnimation>
           <ScrollAnimation
-            animateIn="bounceInRight"
+            animateIn="fadeInRight"
             animateOnce={true}
             duration={1.1}
           >
             <div
               className="flex right"
-              style={{
-                transform: `rotateX(${mouseX * 0.015}deg) rotateY(${
-                  mouseY * -0.015
-                }deg)`,
-              }}
-              onMouseMove={handleMouseMove}
-              onMouseLeave={handleMouseLeave}
+              // style={{
+              //   transform: `rotateX(${mouseX * 0.015}deg) rotateY(${
+              //     mouseY * -0.015
+              //   }deg)`,
+              // }}
+              // onMouseMove={handleMouseMove}
+              // onMouseLeave={handleMouseLeave}
             >
               <div className="tablet-image">
                 <div
@@ -131,45 +140,105 @@ function App() {
 
       <Divider type="content" />
 
-      <section className="content-grid full-width content">
+      <section className="content-grid full-width breakout content">
         <div className="cards grid">
-          <div className="card flex column">
-            <div className="card-icon" />
-            <h1>Donate</h1>
-            Help fund someone to receive health & wellness services and
-            educational classes through the latest technology app.
-            <Button version="card-button">
-              Read More
-              <IoMdArrowRoundUp />
-            </Button>
-          </div>
-          <div className="card flex column">
-            <div className="card-icon" />
-            <h1>Sponsor</h1>
-            We are supported by donations made from sponsors like you. Please
-            help to make a change in someone’s life.
-            <Button version="card-button">
-              Read More
-              <IoMdArrowRoundUp />
-            </Button>
-          </div>
-          <div className="card flex column">
-            <div className="card-icon" />
-            <h1>Community</h1>
-            We know you care about your community and want to make a difference.
-            Contact us and we will provide you with additional info.
-            <Button version="card-button">
-              Read More
-              <IoMdArrowRoundUp />
-            </Button>
-          </div>
+          <ScrollAnimation
+            animateIn="fadeInLeft"
+            animateOnce={true}
+            duration={1.1}
+          >
+            <div className="card donate flex column">
+              <div className="card-icon">
+                <TbMoodDollar />
+              </div>
+              <h1>Donate</h1>
+              Help fund someone to receive health & wellness services and
+              educational classes through the latest technology app.
+              <Button version="card-button">
+                Read More
+                <IoMdArrowRoundUp />
+              </Button>
+              <div className="card-bg-icon">
+                <TbMoodDollar />
+              </div>
+            </div>
+          </ScrollAnimation>
+          <ScrollAnimation
+            animateIn="fadeInUp"
+            animateOnce={true}
+            duration={1.3}
+          >
+            <div className="card sponsor flex column">
+              <div className="card-icon">
+                <TbUserHeart />
+              </div>
+              <h1>Sponsor</h1>
+              We are supported by donations made from sponsors like you. Please
+              help to make a change in someone’s life.
+              <Button version="card-button">
+                Read More
+                <IoMdArrowRoundUp />
+              </Button>
+              <div className="card-bg-icon">
+                <TbUserHeart />
+              </div>
+            </div>
+          </ScrollAnimation>
+          <ScrollAnimation
+            animateIn="fadeInUp"
+            animateOnce={true}
+            duration={1.2}
+          >
+            <div className="card community flex column">
+              <div className="card-icon">
+                <TbUsersGroup />
+              </div>
+              <h1>Community</h1>
+              We know you care about your community and want to make a
+              difference. Contact us and we will provide you with additional
+              info.
+              <Button version="card-button">
+                Read More
+                <IoMdArrowRoundUp />
+              </Button>
+              <div className="card-bg-icon">
+                <TbUsersGroup />
+              </div>
+            </div>
+          </ScrollAnimation>
+
+          <ScrollAnimation
+            animateIn="fadeInRight"
+            animateOnce={true}
+            duration={1.1}
+          >
+            <div className="card mission flex column">
+              <div className="card-icon">
+                <TbHeartFilled />
+              </div>
+              <h1>Mission</h1>
+              We are focused on equity and ensuring that all people have the
+              opportunity to turn their life around and not be left behind.
+              <Button version="card-button">
+                Read More
+                <IoMdArrowRoundUp />
+              </Button>
+              <div className="card-bg-icon">
+                <TbHeartFilled />
+              </div>
+            </div>
+          </ScrollAnimation>
         </div>
+      </section>
+
+      <section className="content-grid full-width">
+        <div>dfjhdfgdfhg</div>
       </section>
 
       <Divider type="footer" />
 
       <footer className="content-grid full-width">
-        <div>Copyright © 2024 Roadway Home. All rights reserved.</div>
+        <div>Copyright © {currentYear} Roadway Home. All rights reserved.</div>
       </footer>
     </main>
   );
